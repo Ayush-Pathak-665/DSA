1092985ca60acb8c573838cb34e9a586767c3429
ayushpathakmca25-cpu <ayush.pathak_mca25@gla.ac.in>
2026-06-09 23:05:18 +0530

    Sorting algos

---
+++ _7Sorting/Bubble.java
@@ -0,0 +1,26 @@
+package _7Sorting;
+
+public class Bubble {
+    public static void sort(int arr[],int n){
+        for(int i = 0;i<n-1;i++){
+            for(int j = 0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(j,arr);
+                }
+            }
+        }
+    }
+    public static void swap(int j,int arr[]){
+        int t = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = t;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        int n = arr.length;
+        sort(arr,n);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
