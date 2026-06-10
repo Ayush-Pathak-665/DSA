cb7ea4eff1d24f9c5753459395ce65f019401fdf
ayushpathakmca25-cpu <ayush.pathak_mca25@gla.ac.in>
2026-06-10 20:20:31 +0530

    Modified Bubble

--- _7Sorting/Bubble.java
+++ _7Sorting/Bubble.java
@@ -4,7 +4,7 @@ public class Bubble {
     public static void sort(int arr[],int n){
         for(int i = 0;i<n-1;i++){
             for(int j = 0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
+                if(arr[j]<arr[j+1]){
                     swap(j,arr);
                 }
             }
@@ -15,8 +15,9 @@ public class Bubble {
         arr[j] = arr[j+1];
         arr[j+1] = t;
     }
+
     public static void main(String[] args) {
-        int arr[] = {5,4,3,2,1};
+        int arr[] = {2,13,-2,-4,7,8};
         int n = arr.length;
         sort(arr,n);
         for (int i : arr) {
