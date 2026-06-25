38bb1639e83bace5df88eac079b26031a13006aa
ayushpathakmca25-cpu <ayush.pathak_mca25@gla.ac.in>
2026-06-25 10:09:11 +0530

    Bubble

--- _7Sorting/Bubble.java
+++ _7Sorting/Bubble.java
@@ -4,7 +4,7 @@ public class Bubble {
     public static void sort(int arr[],int n){
         for(int i = 0;i<n-1;i++){
             for(int j = 0;j<n-i-1;j++){
-                if(arr[j]<arr[j+1]){
+                if(arr[j]>arr[j+1]){
                     swap(j,arr);
                 }
             }
@@ -25,3 +25,4 @@ public class Bubble {
         }
     }
 }
+
